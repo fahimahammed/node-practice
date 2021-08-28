705cc04e79805777a2942623db34d622c8187dbb
fahimahammed <fahimahammed.cse@gmail.com>
2021-08-28 22:40:29 +0600

    fs module

--- fs.js
+++ fs.js
@@ -1,3 +1,19 @@
 const fs = require('fs');
 
-fs.writeFileSync('file.txt', 'Hello, Fahim.');
+
+// write file
+// fs.writeFileSync('file.txt', 'Hello, Fahim.');
+// fs.appendFileSync('file.txt', ' How are you?');
+
+//read file
+
+// way to sync
+const data = fs.readFileSync('file.txt');
+
+console.log(data);  // output buffer data
+console.log(data.toString());  // output string type
+
+//way to aync
+fs.readFile('file.txt', (err, data) => {
+    console.log(data.toString());
+})
