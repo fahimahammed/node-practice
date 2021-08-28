09a5a5ebc8e47494680a788ddc9024a35d700be9
fahimahammed <fahimahammed.cse@gmail.com>
2021-08-28 23:10:26 +0600

    added

--- Lesson-5/event.js
+++ Lesson-5/event.js
@@ -13,3 +13,13 @@ setTimeout(() => {
 }, 2000);
 
 
+// event parameter
+// register a listener for bell ring event
+emitter.on('bellRing', (period) => {
+    console.log(`We need to run because ${period}`);
+});
+
+// raise an event
+setTimeout(() => {
+    emitter.emit('bellRing', 'second period ended.');
+}, 2000);
