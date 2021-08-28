4c29788a8adc86a5479dc7945dc61b8f6285a7f2
fahimahammed <fahimahammed.cse@gmail.com>
2021-08-28 23:35:59 +0600

    create server, run, route

--- Lesson-5/http.js
+++ Lesson-5/http.js
@@ -1,6 +1,24 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){ 
+        res.write('hello programmers!');
+        res.write('How are you?');
+        res.end();
+    }
+    else if (req.url === '/about'){
+        res.write('About page');
+        res.end();
+    }else{
+        res.write('not found');
+        res.end();
+    }
+});
+
+// ignore this
+// server.on('connection', () =>{
+//     console.log('new connection ...');
+// })
 
 server.listen(3000);
 
